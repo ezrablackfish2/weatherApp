8ae298d8fcb02e11a40415cfbbe0afdc5b04d943
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-01-07 10:29:56 +0300

    we are progressing

--- screens/WeatherShowScreen.js
+++ screens/WeatherShowScreen.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const WeatherShowScreen = props => {
 	// this function shows weather up to 5 days from mock API
     
-    return <View>
+    return <View style={styles.container}>
         <Text>Weather Showing screen</Text>
     </View>
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    fontFamily: "Bellota",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+	label : {
+		fontFamily: "Bellota",
+		fontSize: 20,
+	},
+});
 
 export default WeatherShowScreen;
