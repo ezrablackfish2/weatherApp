63c48d6d80ee1fbb9cb21e9eaef31ccf0b3989f7
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-01-07 09:33:56 +0300

    stack worked yay

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { useState, useEffect, useCallback } from "react";
 import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+
+
+import WeatherShowScreen from "./screens/WeatherShowScreen";
+
+
+SplashScreen.preventAutoHideAsync(); // this makes the splash screen overriding it to show
+
+const Stack = createStackNavigator(); // this decalres a stack navigator
 
 
 export default function App() {
@@ -11,11 +23,40 @@ export default function App() {
 	const [appIsLoaded, setAppIsLoaded] = useState(false); //tell if the app is loaded shows the app icon
 
 	useEffect(() => {
-		//this delays and show weather app icon until 2 seconds
-		setTimeout(() => {
-			setAppIsLoaded(true)
-		}, 2000);
-	})
+		const prepare = async() => {
+			// this fetches font from directory and loads it to react native
+			try {
+			await Font.loadAsync({	
+				// this function loads the fonts
+				"Bellota": require("./assets/fonts/Bellota-Regular.ttf"),
+				"Aguafina": require("./assets/fonts/AguafinaScript-Regular.ttf"),
+				"Barrio": require("./assets/fonts/Barrio-Regular.ttf"),
+				"Amatic": require("./assets/fonts/AmaticSC-Regular.ttf"),
+				"BlackOps": require("./assets/fonts/BlackOpsOne-Regular.ttf"),
+				"Caveat": require("./assets/fonts/Caveat-VariableFont_wght.ttf"),
+				"Chakra": require("./assets/fonts/ChakraPetch-Regular.ttf"),
+				"Croissant": require("./assets/fonts/CroissantOne-Regular.ttf"),
+				"Dancing": require("./assets/fonts/DancingScript-VariableFont_wght.ttf"),
+				"Hammer": require("./assets/fonts/HammersmithOne-Regular.ttf"),
+				"Itim": require("./assets/fonts/Itim-Regular.ttf"),
+				"Lilita": require("./assets/fonts/LilitaOne-Regular.ttf"),
+				"Montserrat": require("./assets/fonts/Montserrat-VariableFont_wght.ttf"),
+				"Orbitron": require("./assets/fonts/Orbitron-VariableFont_wght.ttf"),
+				"Rajdhani": require("./assets/fonts/Rajdhani-Regular.ttf"),
+				"SedgwickAve": require("./assets/fonts/SedgwickAve-Regular.ttf"),
+				"Shadows": require("./assets/fonts/ShadowsIntoLight-Regular.ttf"),
+			})
+			}
+			catch (error) {
+				console.log(error);
+			}
+			finally {
+				setAppIsLoaded(true);
+			}		
+		};
+		prepare();
+	}, [])
+
 
 	const onLayout = useCallback(async () => {
 		if (appIsLoaded) {
@@ -41,6 +82,13 @@ export default function App() {
 
       <Text style = {styles.label}>Weather</Text>
 
+		<NavigationContainer>
+	      <Stack.Navigator>
+      		<Stack.Screen name="Home" component={ WeatherShowScreen } />
+	  	</Stack.Navigator>
+
+		</NavigationContainer>
+
 
       <StatusBar style="auto" />
 	  </SafeAreaView>
@@ -57,6 +105,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   label : {
+	  fontFamily: "Rajdhani",
   },
 	weather : {
 	}
---
+++ screens/WeatherShowScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const WeatherShowScreen = props => {
+	// this function shows weather up to 5 days from mock API
+    
+    return <View>
+        <Text>Weather Showing screen</Text>
+    </View>
+};
+
+export default WeatherShowScreen;
