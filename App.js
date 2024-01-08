28514f32d6e1d71dd20993aadbce4d1c2bc1d619
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-01-08 11:07:26 +0300

    refactoring part 1 done

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
+import { loadFonts } from './fonts.js';
+
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
@@ -58,40 +60,9 @@ export default function App() {
 	// this is where the weather app works this is the root function
   const [appIsLoaded, setAppIsLoaded] = useState(false);  //tell if the app is loaded shows the app icon 
 
-  useEffect(() => {
-    async function loadFonts() {
-		// this fetches font from directory and lddoads it to react native
-      try {
-        await Font.loadAsync({
-		// this function loads the fonts
-				"Bellota": require("./assets/fonts/Bellota-Regular.ttf"),
-				"Aguafina": require("./assets/fonts/AguafinaScript-Regular.ttf"),
-				"Barrio": require("./assets/fonts/Barrio-Regular.ttf"),
-				"Amatic": require("./assets/fonts/AmaticSC-Regular.ttf"),
-				"BlackOps": require("./assets/fonts/BlackOpsOne-Regular.ttf"),
-				"Caveat": require("./assets/fonts/Caveat-VariableFont_wght.ttf"),
-				"Chakra": require("./assets/fonts/ChakraPetch-Regular.ttf"),
-				"Croissant": require("./assets/fonts/CroissantOne-Regular.ttf"),
-				"Dancing": require("./assets/fonts/DancingScript-VariableFont_wght.ttf"),
-				"Hammer": require("./assets/fonts/HammersmithOne-Regular.ttf"),
-				"Itim": require("./assets/fonts/Itim-Regular.ttf"),
-				"Lilita": require("./assets/fonts/LilitaOne-Regular.ttf"),
-				"Montserrat": require("./assets/fonts/Montserrat-VariableFont_wght.ttf"),
-				"Orbitron": require("./assets/fonts/Orbitron-VariableFont_wght.ttf"),
-				"Rajdhani": require("./assets/fonts/Rajdhani-Regular.ttf"),
-				"SedgwickAve": require("./assets/fonts/SedgwickAve-Regular.ttf"),
-				"Shadows": require("./assets/fonts/ShadowsIntoLight-Regular.ttf"),
-	  
-        });
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setAppIsLoaded(true);
-      }
-    }
-    loadFonts();
+    useEffect(() => {
+	  loadFonts(setAppIsLoaded);
   }, []);
-
   const onLayout = useCallback(async () => {
     if (appIsLoaded) {
       await SplashScreen.hideAsync();  // hides the app icon
